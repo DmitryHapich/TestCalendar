474ed42f881e69e24dfc1996dfc79280332d572c
Dima Hapich <dmitry.hapich@requestum.com>
2026-04-13 16:10:27 +0300

    Initial Commit

---
+++ TestCalendar/TestCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCalendarApp.swift
+//  TestCalendar
+//
+//  Created by Dima H on 13.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct TestCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
